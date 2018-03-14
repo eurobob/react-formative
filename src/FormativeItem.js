5a52094facd680c37de2abc200e74c576abddbfe
eurobob <rob@writerhythm.com>
2018-03-14 15:40:03 +0700

    Boy, that escalated quickly! Sloppy early FormativeItem implementation of standard inputs and customisation with third party components like redux form

--- src/FormativeItem.js
+++ src/FormativeItem.js
@@ -1,12 +1,81 @@
 import React from 'react';
+import PropTypes from 'proptypes';
+
+const DOMExclusions = ['fElement', 'fHeading', 'fComponent', 'fFields'];
 
 class FormativeItem extends React.Component {
   constructor() {
     super();
   }
+  validateChildProps(props) {
+    return Object.keys(props).reduce((acc, key) => {
+      if (key && DOMExclusions.indexOf(key) < 0) {
+        return Object.assign(acc, { [key]: props[key] });
+      }
+      return acc;
+    }, {});
+  }
   render() {
-    return <div>{this.props.children}</div>;
+    const {
+      fHeading,
+      fComponent,
+      fElement,
+      name,
+      value,
+      label,
+      type,
+      component,
+    } = this.props;
+    const childProps = this.validateChildProps(this.props);
+
+    let child;
+    if (fComponent) {
+      child = React.createElement(fComponent, childProps);
+    } else {
+      child = React.createElement(fElement, childProps);
+    }
+
+    return (
+      <div>
+        <h2>{fHeading}</h2>
+        {(type === 'checkbox' || type === 'radio') && (
+          <label>
+            {child}
+            {label || name}
+          </label>
+        )}
+        {type !== 'checkbox' &&
+          type !== 'radio' && (
+            <label>
+              {label || name}
+              {child}
+            </label>
+          )}
+      </div>
+    );
   }
 }
+
+FormativeItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  value: PropTypes.string,
+  type: PropTypes.string,
+  fElement: PropTypes.string,
+  fHeading: PropTypes.string,
+  fComponent: PropTypes.func,
+  fFields: PropTypes.array,
+};
+
+FormativeItem.defaultProps = {
+  name: '',
+  label: null,
+  value: '',
+  type: 'text',
+  fElement: 'input',
+  fHeading: null,
+  fComponent: null,
+  fFields: [],
+};
 
 export default FormativeItem;
