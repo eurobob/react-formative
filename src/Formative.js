752f90b858d2957ed679ae7ba4cc47358f905f53
eurobob <rob@writerhythm.com>
2018-04-21 11:22:06 +0700

    form -> review animations

--- src/Formative.js
+++ src/Formative.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import PropTypes from 'proptypes';
-import update from 'immutability-helper';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import React from "react";
+import PropTypes from "proptypes";
+import update from "immutability-helper";
+import { Transition, TransitionGroup, CSSTransition } from "react-transition-group";
 
-import FormativeCounter from './FormativeCounter';
-import FormativeNavigation from './FormativeNavigation';
-import FormativeProgress from './FormativeProgress';
-import FormativeItem from './FormativeItem';
-import FormativeReview from './FormativeReview';
+import FormativeCounter from "./FormativeCounter";
+import FormativeNavigation from "./FormativeNavigation";
+import FormativeProgress from "./FormativeProgress";
+import FormativeItem from "./FormativeItem";
+import FormativeReview from "./FormativeReview";
 
 const childFactoryCreator = classNames => child =>
   React.cloneElement(child, {
-    classNames,
+    classNames
   });
 
 class Formative extends React.Component {
@@ -21,7 +21,8 @@ class Formative extends React.Component {
       index: 0,
       total: 0,
       fields: props.fields,
-      animationClass: 'from-bottom',
+      animationClass: "from-bottom",
+      review: false
     };
     this.nextField = this.nextField.bind(this);
     this.prevField = this.prevField.bind(this);
@@ -31,96 +32,129 @@ class Formative extends React.Component {
   nextField() {
     // @TODO double check if user has gone back
     if (this.state.fields[this.state.index].value) {
-      this.setState({
-        index: this.state.index + 1,
-        total: this.state.total + 1,
-        animationClass: 'from-bottom',
-      });
+      this.setState(
+        {
+          index: this.state.index + 1,
+          total: this.state.total + 1,
+          animationClass: "from-bottom"
+        },
+        () => {
+          if (this.state.index === this.state.fields.length) {
+            const this2 = this;
+            setTimeout(() => {
+              this2.setState({
+                review: true
+              });
+            }, 1000);
+          }
+        }
+      );
     }
   }
   prevField() {
     this.setState(
       {
-        animationClass: 'from-top',
+        animationClass: "from-top"
       },
       () => {
         this.setState({
-          index: this.state.index - 1,
+          index: this.state.index - 1
         });
-      },
+      }
     );
   }
   handleChange(event, index) {
     this.setState({
       fields: update(this.state.fields, {
         [index]: {
-          value: { $set: event.target.value },
-        },
-      }),
+          value: { $set: event.target.value }
+        }
+      })
     });
   }
   navigate(index) {
     let animationClass;
     if (index < this.state.index) {
-      animationClass = 'from-top';
+      animationClass = "from-top";
     } else if (index > this.state.index) {
-      animationClass = 'from-bottom';
+      animationClass = "from-bottom";
     }
     this.setState({ index, animationClass });
   }
   render() {
-    const { index, fields, total, animationClass } = this.state;
+    const { index, fields, total, animationClass, review } = this.state;
+    const isFinished = index === fields.length;
 
-    if (this.state.index === this.state.fields.length) {
+    if (review) {
       return (
-        <FormativeReview
-          fields={fields}
-          className={`${this.props.className} f-c-form`}
-          handleChange={this.handleChange}
-          onSubmit={this.props.onSubmit}
-        />
+        <TransitionGroup className={this.props.className}>
+          <CSSTransition
+            key={index}
+            classNames="f-a-from-bottom-"
+            timeout={500}
+            mountOnEnter={true}
+            unmountOnExit={true}
+            appear={true}
+          >
+            <FormativeReview
+              fields={fields}
+              className={`${this.props.className} f-c-form`}
+              handleChange={this.handleChange}
+              onSubmit={this.props.onSubmit}
+            />
+          </CSSTransition>
+        </TransitionGroup>
       );
     }
 
     return (
       <form className={`${this.props.className} f-c-form`}>
-        <FormativeCounter
-          fields={fields}
-          index={index}
-          animationClass={animationClass}
-        />
-        <FormativeProgress fields={fields} total={total} />
-        <FormativeNavigation
-          fields={fields}
-          index={index}
-          total={total}
-          navigate={this.navigate}
-        />
-        <TransitionGroup
-          childFactory={childFactoryCreator(`f-a-${animationClass}-`)}
-          component="ul"
-          className="f-c-list">
-          <CSSTransition
-            key={index}
-            classNames={`f-a-${animationClass}-`}
-            timeout={500}
-            mountOnEnter={true}
-            unmountOnExit={true}>
-            <FormativeItem
-              {...fields[index]}
-              key={index}
-              nextField={this.nextField}
-              handleChange={event => this.handleChange(event, index)}
-            />
-          </CSSTransition>
-        </TransitionGroup>
-        <button
-          className="f-c-button f-c-button--continue"
-          type="button"
-          onClick={this.nextField}
-          disabled={!this.state.fields[this.state.index].value}>
-          Continue
-        </button>
+        <div className={`f-c-form__inner ${isFinished ? 'f-a-fade-exit f-a-fade-exit-active' : ''}`}>
+          <FormativeCounter
+            fields={fields}
+            index={index}
+            animationClass={animationClass}
+          />
+          <FormativeProgress fields={fields} total={total} />
+          <FormativeNavigation
+            fields={fields}
+            index={index}
+            total={total}
+            navigate={this.navigate}
+          />
+          <TransitionGroup
+            childFactory={childFactoryCreator(`f-a-${animationClass}-`)}
+            component="ul"
+            className="f-c-list"
+          >
+            {fields[index] &&
+              <CSSTransition
+                key={index}
+                classNames={`f-a-${animationClass}-`}
+                timeout={500}
+                mountOnEnter={true}
+                unmountOnExit={true}
+              >
+                  <FormativeItem
+                    {...fields[index]}
+                    key={index}
+                    nextField={this.nextField}
+                    handleChange={event => this.handleChange(event, index)}
+                  />
+              </CSSTransition>}
+          </TransitionGroup>
+          <button
+            className="f-c-button f-c-button--continue"
+            type="button"
+            onClick={this.nextField}
+            disabled={
+              this.state.fields[this.state.index] &&
+              !this.state.fields[this.state.index].value
+            }
+          >
+            Continue
+          </button>
+        </div>
       </form>
     );
   }
@@ -128,12 +162,12 @@ class Formative extends React.Component {
 
 Formative.propTypes = {
   animated: PropTypes.bool,
-  animation: PropTypes.string,
+  animation: PropTypes.string
 };
 
 Formative.defaultProps = {
   animated: false,
-  animation: 'fade',
+  animation: "fade"
 };
 
 export default Formative;
--- src/FormativeCounter.js
+++ src/FormativeCounter.js
@@ -7,24 +7,34 @@ const childFactoryCreator = classNames => child =>
   });
 
 function FormativeCounter(props) {
-  return (
-    <div className="f-c-counter">
-      <TransitionGroup
-        childFactory={childFactoryCreator(`f-a-${props.animationClass}-`)}
-        component="span"
-        className="f-c-counter__incrementor">
-        <CSSTransition
-          key={props.index}
-          classNames={`f-a-${props.animationClass}-`}
-          timeout={500}
-          mountOnEnter={true}
-          unmountOnExit={true}>
-          <span className="f-c-counter__number">{props.index + 1}</span>
-        </CSSTransition>
-      </TransitionGroup>
-      <span>{` / ${props.fields.length}`}</span>
-    </div>
-  );
+  if (props.index < props.fields.length) {
+    return (
+      <div className="f-c-counter">
+        <TransitionGroup
+          childFactory={childFactoryCreator(`f-a-${props.animationClass}-`)}
+          component="span"
+          className="f-c-counter__incrementor">
+            <CSSTransition
+              key={props.index}
+              classNames={`f-a-${props.animationClass}-`}
+              timeout={500}
+              mountOnEnter={true}
+              unmountOnExit={true}>
+              <span className="f-c-counter__number">{props.index + 1}</span>
+            </CSSTransition>
+        </TransitionGroup>
+        <span>{` / ${props.fields.length}`}</span>
+      </div>
+  )} else {
+    return (
+      <div className="f-c-counter">
+        <span className="f-c-counter__incrementor">
+          <span className="f-c-counter__number">{props.fields.length}</span>
+        </span>
+        <span>{` / ${props.fields.length}`}</span>
+      </div>
+    )
+  };
 }
 
 export default FormativeCounter;
